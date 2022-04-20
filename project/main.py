24d16c23536e9fc90c8e06a7c20a59b0c53bd0bb
samirakhalil1993 <103117504+samirakhalil1993@users.noreply.github.com>
2022-04-20 13:33:47 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -9,60 +9,81 @@ if __name__ == '__main__':
     sys.exit(main())
 
 
-# Import libraries
-
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor
-from pybricks.parameters import Port, Button
-from pybricks.robotics import DriveBase
-
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
 ev3 = EV3Brick()
 
-# Vilka portar som hör till vad
-Left_drive=Port.C 
-Right_drive=Port.B
-Crane_motor=Port.A
-Front_button=Port.S1
-Light_sensor=Port.S3
-Ultrasonic_sensor=Port.S4
 
-# Definierar roboten
-robot = DriveBase(Left_drive, Right_drive, wheel_diameter=56, axle_track=118)
+# Write your program here.
 
-# Driving variables
-speed = 100
-speed_modifier_while_turning = 0.8
-turn_intensity = 90
-light_sensitivity = 20 
+# Motor definitions
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
+crane_motor= Motor(Port.A)
+robot = DriveBase(left_motor, right_motor, wheel_diameter= 56, axle_track= 120)
+# Sensor definitions
+left_light = ColorSensor(Port.S3)
+ultra_sensor = UltrasonicSensor(Port.S4)
+touch_sensor = TouchSensor(Port.S1)
+# Your code goes here
 
-# Körloopen
+#Declaring some varaibles for the robot
+running = True
+
+# line_color = Color.BLACK #Chosing to only detect black as the color
+# floor_color = Color.WHITE #Floor is the white color
+BLACK = 9
+WHITE = 85
+color_between = (BLACK + WHITE) / 2
+default_speed = -100 #mm/s
+PROPORTUINAL_GAIN = 1.2
+#Making the robot move at normal speed
+
+# left_motor.dc(default_speed)
+# right_motor.dc(default_speed)
+
+#Program loop
+
+while running:
+    deviation = left_ligt.reflection() - color_between
+    turn_rate = PROPORTIONAL_GAIN * deviation  
+    while touch_sensor.pressed():
+        left_motor.stop()
+        right_motor.stop() 
+    while ultra_sensor.distance() < 100:
+        left_motor.stop()
+        right_motor.stop()     
+    while left_light.reflection() == line_color:
+        robot.drive(default_speed, turn_rate)
+    whait(5)
+    robot.stop()
+    right_motor.brake()
+    left_motor.brake()
+        
+"""PROPORTIONAL_GAIN = 1.2
+
+# Start following the line endlessly.
 while True:
-    # Sväng vänster
-    if Light_sensor.reflection() < light_sensitivity:
+    # Calculate the deviation from the threshold.
+    deviation = line_sensor.reflection() - threshold
 
-        # Intensity upskattar hur skarp svängen är
-        intensity = (100 -Light_sensor.reflection()) / 100
+    # Calculate the turn rate.
+    turn_rate = PROPORTIONAL_GAIN * deviation
 
-        # Sänker hastigheten i skarpa kurvor
-        speed_mod = 1 - intensity
+    # Set the drive base speed and turn rate.
+    robot.drive(DRIVE_SPEED, turn_rate)
 
-        # Sätter in alla värden
-        # Första värdet är hastigheten, andra värdet är hur mycket roboten ska svänga
-        robot.drive(speed * speed_mod * speed_modifier_while_turning, -turn_intensity * intensity)
-
-    
-
-    elif Ultrasonic_sensor.distance() < 100:
-            Left_drive.stop()
-            Right_drive.stop()
-
-    elif Front_button.pressed():
-        Crane_motor == (Button.LEFT_UP, Button.LEFT_DOWN)
-        reversing = False
-        robot.stop()
-        Left_drive.run(speed=100)
-        Right_drive.run(speed=100)
-
-    else:
-
-        robot.drive(speed, 0)
+    # You can wait for a short time or do other things in this loop.
+    wait(10)"""
