f88e14da1b92072e254220d443332cc98ef33ec4
samirakhalil1993 <103117504+samirakhalil1993@users.noreply.github.com>
2022-05-18 08:25:08 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+import sys
+import __init__
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
