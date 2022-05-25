367e5e558850eb05f339717db8f06c1784bed8a2
samirakhalil1993 <103117504+samirakhalil1993@users.noreply.github.com>
2022-05-25 13:42:32 +0200

    gubran

--- project/setup.py
+++ project/setup.py
@@ -1,26 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
 
-left_motor = Motor(Port.A) 
-right_motor = Motor(Port.B)
-robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=152) 
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
 
 
+# Create your objects here.
+ev3 = EV3Brick()
 
-light = ColorSensor(Port.S1)
-reflection = 60
-speed =300
-yellow_line = Color.YELLOW
 
-while True :
-    if  yellow_line:
-        mod_speed = speed/2
-        correction = (reflection - light.reflection()) *2*speed/150
-        robot.drive(mod_speed ,correction)
-    else:
-        robot.drive(speed ,0)
+# Write your program here.
+ev3.speaker.beep()
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+ultra_sensor = UltrasonicSensor(Port.S4)
+#Crane_motor=Port.A
+Front_button=Port.S1
+crane_motor= Motor(Port.A)
+touch_sensor = TouchSensor(Port.S1)
+# Initialize the color sensor.
+line_sensor = ColorSensor(Port.S3)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
